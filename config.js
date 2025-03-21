764555ea3e8b190cd91df4e5a2077a462986bb96
nommer3 <botdeploiement@gmail.com>
2025-03-21 19:24:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -19,23 +19,23 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "eFojRYab#xScB7iquVJHvHOA6LcQDWgKgRR88TF7EXPjTO6Wxjqs",
+SESSION_ID: process.env.SESSION_ID || "KERM-MD-V1~OIADHAia#hXW5e-H7Qsnkb1CRqOESLMhtya7aRTNV0Bb2hddTq98",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://www.reddit.com/r/ClassroomOfTheElite/comments/vrlwif/kiyotaka_ayanokoji_liraletter/?tl=fr",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://images.app.goo.gl/w5gS",
 ALIVE_MSG: process.env.ALIVE_MSG || "_Hi 💁🏽 How Can I Assist You. Am alive Now._",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
-PREFIX: process.env.PREFIX || ".",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+PREFIX: process.env.PREFIX || "✓",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
 FAKE_TYPING: process.env.FAKE_TYPING || "false",
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
-CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
-AUTO_REACT: process.env.AUTO_REACT || "false",
-HEART_REACT: process.env.HEART_REACT || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+CURRENT_STATUS: process.env.CURRENT_STATUS || "true"
+AUTO_REACT: process.env.AUTO_REACT || "false
+HEART_REACT: process.env.HEART
 OWNER_REACT: process.env.OWNER_REACT || "false",
 BOT_NAME: process.env.BOT_NAME || "『🌹𝑸𝑼𝑬𝑺𝑰𝑨🌹 𝗩1』",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
